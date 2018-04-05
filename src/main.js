7e30438c506a636fc33987e4c526621abfa00e20
Dacrol <jonas.ahrens@gmail.com>
2018-04-05 16:59:23 +0200

    Register Upload component

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { apolloProvider } from './vue-apollo'
+import { Upload } from 'element-ui';
 
 Vue.config.productionTip = false
+
+Vue.component('element-upload', Upload)
 
 new Vue({
   provide: apolloProvider.provide(),
